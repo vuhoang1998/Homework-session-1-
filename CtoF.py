3b02b4e473d6c621dc22f56937a06f5b90d81d5a
vuhoang1998 <rabac1998@gmail.com>
2016-11-20 22:31:52 +0700

    Add files via upload

---
+++ CtoF.py
@@ -0,0 +1,3 @@
+m = int(input("Enter the temperature in Celsius?"))
+n = m*1.8 + 32
+print(m,"(C) =",n,"(F)")
---
+++ rabbits.py
@@ -0,0 +1,9 @@
+m = 1
+n = 0
+p = 0
+for i in range(4) :
+    print("Month",i,":",m,"pair(s) of rabbits")
+    p = n
+    n = m
+    m = n+p
+    
