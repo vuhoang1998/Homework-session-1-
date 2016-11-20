39924449c1475d1973fcb15547634404f3b897e8
vuhoang1998 <rabac1998@gmail.com>
2016-11-21 01:26:26 +0700

    Update rabbits.py
    fixed again

--- rabbits.py
+++ rabbits.py
@@ -2,8 +2,9 @@ m = 1
 n = 0
 p = 0
 for i in range(4) :
+    print("Month",i,":",m,"pair(s) of rabbits")
     p = n
     n = m
     m = n+p
-    print("Month",i,":",m,"pair(s) of rabbits")
+    
     
